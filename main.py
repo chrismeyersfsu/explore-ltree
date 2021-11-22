bcd11ac183296ff446689aaeba4172539ebfbb25
chris meyers <chris.meyers.fsu@gmail.com>
2021-11-21 19:44:10 -0500

    cleanup
    * move inv.sql code into main.py

--- main.py
+++ main.py
@@ -8,6 +8,15 @@ cur = conn.cursor()
 
 cur.execute(open("inv.sql", "r").read())
 
+cur.execute("""select abc.node_id, abc.name, abc.path, kv.key k, kv.value v
+    INTO TEMP TABLE path_recent FROM
+    (select DISTINCT ON (t.node_id)
+    t.id, t.node_id, n.name, t.path, n.vars
+    FROM tree t, node n
+    WHERE (n.vars ? 'can_eat' OR n.vars ? 'is_pet' OR n.vars ? 'should_eat')
+    AND t.node_id = n.id) AS abc, jsonb_each(abc.vars) as kv
+    WHERE kv.key IN ('can_eat', 'is_pet', 'should_eat');""")
+
 cur.execute('select p1.node_id, p1.name, p1.path, p1.k, p1.v from path_recent p1;')
 for r in cur.fetchall():
     print(r)
